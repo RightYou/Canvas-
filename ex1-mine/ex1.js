9e8f31806f0fc44a2788c98ee0d3a7e61a12a097
RightYou <2210753946@qq.com>
2019-10-16 14:28:28 +0800

    代码风格优化

--- ex1-mine/ex1.js
+++ ex1-mine/ex1.js
@@ -1,6 +1,6 @@
 /*
     Function:Ex1
-    Author  :unowen
+    Author  :Un-owen
     BuildData:2019-10-16
     Version:Alpha
  */
@@ -9,7 +9,7 @@ var canvas = document.getElementById("MyCanvas");
 //获取canvas的绘图环境对象context
 var ctx = canvas.getContext("2d");
 //修改canvas 上的字体大小
-ctx.font="64px 宋体";
+ctx.font = "64px 宋体";
 //修改文本绘制点的水平方向
 ctx.textAlign = "center";
 //修改文本绘制点的字符基线方向（垂直方向）
@@ -17,4 +17,4 @@ ctx.textBaseline = "middle";
 //修改填充文本的颜色
 ctx.fillStyle = '#ffcc33';
 //绘制填充文本
-ctx.fillText("开学第一课",canvas.width/2,canvas.height/2);
+ctx.fillText("开学第一课", canvas.width / 2, canvas.height / 2);
