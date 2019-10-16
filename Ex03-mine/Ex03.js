49028aa552832a43a272bb2efe24cdb00778d718
RightYou <2210753946@qq.com>
2019-10-16 15:04:47 +0800

    Ex033 commit

---
+++ Ex03-mine/Ex03.js
@@ -0,0 +1,16 @@
+/*
+    Function:Ex03
+    Author:Un-owen
+    BuildDate:2019-10-16
+    Version:Alpha
+ */
+
+var canvas = document.getElementById("MyCanvas");
+var ctx = canvas.getContext("2d");
+
+var point={x:canvas.width/2,y:canvas.height/2};
+
+var style={
+    fontSize:60,
+    hAlign:"center",
+}
